fbc909dceb341fde6ddb250467bc40b4667e4cb9
Jose <jose.domingo@ipilyon.net>
2020-01-21 11:38:37 +0100

    First commit TP Gestion exceptions

---
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -0,0 +1,181 @@
+package com.ipiecoles.java.java230;
+
+import com.ipiecoles.java.java230.exceptions.BatchException;
+import com.ipiecoles.java.java230.model.Employe;
+import com.ipiecoles.java.java230.repository.EmployeRepository;
+import com.ipiecoles.java.java230.repository.ManagerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+import org.joda.time.format.DateTimeFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class MyRunner implements CommandLineRunner {
+
+    private static final String REGEX_MATRICULE = "^[MTC][0-9]{5}$";
+    private static final String REGEX_NOM = ".*";
+    private static final String REGEX_PRENOM = ".*";
+    private static final int NB_CHAMPS_MANAGER = 5;
+    private static final int NB_CHAMPS_TECHNICIEN = 7;
+    private static final String REGEX_MATRICULE_MANAGER = "^M[0-9]{5}$";
+    private static final String REGEX_MATRICULE_COMMERCIAL = "^C[0-9]{5}$";
+    private static final int NB_CHAMPS_COMMERCIAL = 7;
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    @Autowired
+    private ManagerRepository managerRepository;
+
+    private List<Employe> employes = new ArrayList<Employe>();
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void run(String... strings) throws Exception {
+        String fileName = "employes.csv";
+        readFile(fileName);
+    }
+
+    /**
+     * Méthode qui lit le fichier CSV en paramètre afin d'intégrer son contenu en BDD
+     * @param fileName Le nom du fichier (à mettre dans src/main/resources)
+     * @return une liste contenant les employés à insérer en BDD ou null si le fichier n'a pas pu être le
+     */
+    public List<Employe> readFile(String fileName) throws Exception {
+        Stream<String> stream;
+        stream = Files.lines(Paths.get(new ClassPathResource(fileName).getURI()));
+
+        Integer i = 0;
+
+        for(String ligne : stream.collect(Collectors.toList())) {
+            i++;
+            try {
+                processLine(ligne);
+            } catch (BatchException e){
+                System.out.println("Ligne " + i + " : " + e.getMessage() + " => " + ligne);
+            }
+        }
+        return employes;
+    }
+
+    /**
+     * Méthode qui regarde le premier caractère de la ligne et appelle la bonne méthode de création d'employé
+     * @param ligne la ligne à analyser
+     * @throws BatchException si le type d'employé n'a pas été reconnu
+     */
+    private void processLine(String ligne) throws BatchException, ParseException {
+        String[] splitLigne = ligne.split(",");
+        if (splitLigne.length >= 5) {
+            if (!splitLigne[0].matches("^[MTC]{1}.*")) {
+                throw new BatchException("Type d'employe inconnu : " + ligne.charAt(0));
+            }
+            if (!splitLigne[0].matches(REGEX_MATRICULE)) {
+                throw new BatchException("La chaîne " + splitLigne[0] + " ne respecte pas l'expression régulière ^[MTC][0-9]{5}$");
+            }
+            processManager(ligne);
+            processCommercial(ligne);
+            processTechnicien(ligne);
+            String date = splitLigne[3];
+            try {
+                DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(date);
+            } catch (Exception e) {
+                throw new BatchException(splitLigne[3] + " ne respecte pas le format de date dd/MM/yyyy");
+            }
+            String salaire = splitLigne[4];
+            try {
+                Double.parseDouble(salaire);
+            } catch (Exception e) {
+                throw new BatchException(salaire + " n'est pas un nombre valide pour un salaire");
+            }
+
+        }
+    }
+
+    /**
+     * Méthode qui crée un Commercial à partir d'une ligne contenant les informations d'un commercial et l'ajoute dans la liste globale des employés
+     * @param ligneCommercial la ligne contenant les infos du commercial à intégrer
+     * @throws BatchException s'il y a un problème sur cette ligne
+     */
+    private void processCommercial(String ligneCommercial) throws BatchException {
+        String[] splitLigneCommercial = ligneCommercial.split(",");
+        if (splitLigneCommercial[0].matches(REGEX_MATRICULE_COMMERCIAL)) {
+            if (splitLigneCommercial.length != NB_CHAMPS_COMMERCIAL) {
+                throw new BatchException("La ligne commercial ne contient pas 7 éléments mais " + splitLigneCommercial.length);
+            }
+            String CaCommercial = splitLigneCommercial[5];
+            try {
+                Double.parseDouble(CaCommercial);
+            } catch (Exception e) {
+                throw new BatchException("Le chiffre d'affaire du commercial est incorrect : " + CaCommercial);
+            }
+            String PerfCommercial = splitLigneCommercial[6];
+            try {
+                Integer.parseInt(PerfCommercial);
+            } catch (Exception e) {
+                throw new BatchException("La performance du commercial est incorrecte : " + PerfCommercial);
+            }
+        }
+    }
+
+    /**
+     * Méthode qui crée un Manager à partir d'une ligne contenant les informations d'un manager et l'ajoute dans la liste globale des employés
+     * @param ligneManager la ligne contenant les infos du manager à intégrer
+     * @throws BatchException s'il y a un problème sur cette ligne
+     */
+    private void processManager(String ligneManager) throws BatchException {
+        String[] splitLigneManager = ligneManager.split(",");
+        if (splitLigneManager[0].matches(REGEX_MATRICULE_MANAGER)) {
+            if (splitLigneManager.length != NB_CHAMPS_MANAGER) {
+                throw new BatchException("La ligne manager ne contient pas 5 éléments mais " + splitLigneManager.length);
+            }
+        }
+    }
+
+    /**
+     * Méthode qui crée un Technicien à partir d'une ligne contenant les informations d'un technicien et l'ajoute dans la liste globale des employés
+     * @param ligneTechnicien la ligne contenant les infos du technicien à intégrer
+     * @throws BatchException s'il y a un problème sur cette ligne
+     */
+    private void processTechnicien(String ligneTechnicien) throws BatchException {
+        String[] splitLigneTechnicien = ligneTechnicien.split(",");
+        if (splitLigneTechnicien[0].matches("^T{1}.*")) {
+            if (splitLigneTechnicien.length != NB_CHAMPS_TECHNICIEN) {
+                throw new BatchException("La ligne technicien ne contient pas 7 éléments mais " + splitLigneTechnicien.length);
+            }
+            if (!splitLigneTechnicien[6].matches(REGEX_MATRICULE_MANAGER)) {
+                throw new BatchException("La chaine " + splitLigneTechnicien[6] + " ne respecte pas l'expression régulière ^M[0-9]{5}$");
+            }
+            String gradeTechnicien = splitLigneTechnicien[5];
+            try {
+                Integer.parseInt(gradeTechnicien);
+            } catch (Exception e) {
+                throw new BatchException("Le grade du technicien est incorrect : " + gradeTechnicien);
+            }
+            if (Integer.parseInt(gradeTechnicien) < 1 || Integer.parseInt(gradeTechnicien) > 5) {
+                throw new BatchException("Le grade doit être compris entre 1 et 5 : " + gradeTechnicien + ", technicien : Technicien{grade=null} Employe{nom='null', prenom='null', matricule='null', dateEmbauche=null, salaire=1480.27}");
+            }
+            try {
+
+            }
+        }
+    }
+
+}
